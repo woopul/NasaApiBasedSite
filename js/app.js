5c1d6099772d8574271626bf98f26a94fcaa2fbe
Wojtek Opulski <wojtekopulski@gmail.com>
2018-08-10 00:30:05 +0200

    some code trimming, checking ajax request with diffrent date

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
 //https://api.nasa.gov/planetary/apod?api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT
 //KEY: key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT
 
-var apodUrl = 'https://api.nasa.gov/planetary/apod?api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT';
+const apodUrl = 'https://api.nasa.gov/planetary/apod?api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT',
+    roverUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
+
+let currentPicDate = '';
 
 
 // ---------------------RUN
 
 
 showAPOD();
+testDays();
+console.log(currentPicDate);
 
 
+//-----------------AJAX
 
-//-----------------function declarations
-
-function showAPOD() {
+function showAPOD(dateOfPic) {
     $.ajax({
         method: 'GET',
         url: apodUrl,
         dataType: 'json',
-        date: '2018 August 07'
     }).done(function (response) {
         injectApod(response);
         console.log(response);
@@ -27,27 +30,56 @@ function showAPOD() {
     });
 }
 
+function testDays() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.nasa.gov/planetary/apod?date=2014-10-01&api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT',
+        dataType: 'json',
+    }).done(function (response) {
+        // injectApod(response);
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
 
+function showRoverAPI() {
+    $.ajax({
+        method: 'GET',
+        url: roverUrl,
+        dataType: 'json',
+        // date: '2018 August 07'
+    }).done(function (response) {
+        injectApod(response);
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
+
+function changePic() {
+
+}
+
+
+//------------------------function declarations
+
+//takes APOD OBJ (pic url, title, explanation, date) and put each of these data in its' place
 function injectApod(apodObj) {
-    var sectionWelcome = $('section.welcome'),
-        pictureTitle = $('#picTitle'),
-        pictureDate = $('#date'),
-        pictureExplenation = $('#explanation');
-
-
     var backgroundImg = apodObj.url,
         backgrounImgHd = apodObj.hdurl,
         apodDate = apodObj.date,
         apodPictureTitle = apodObj.title,
         apodPictureExplanation = apodObj.explanation;
 
+    currentPicDate = apodDate;
 
-    console.log(backgroundImg);
-    pictureTitle.text(apodPictureTitle);
-    pictureDate.text(apodDate);
-    pictureExplenation.text(apodPictureExplanation);
+    $('section.welcome').css('background-image', 'url(' + backgroundImg + ')');
+    $('#picTitle').text(apodPictureTitle)
+    $('#date').text(apodDate);
+    $('#explanation').text(apodPictureExplanation);
 
-    sectionWelcome.css('background-image','url('+backgroundImg+')');
-    // sectionWelcome.css('background' , 'green');
-    // $('section.welcome').css('background-img', 'url('+backgroundImg+')');
+    console.log(currentPicDate);
+    console.log([...currentPicDate]);
+
 }
