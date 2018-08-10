1640be493300761ecb71b902aafe2065bab21dd4
Wojtek Opulski <wojtekopulski@gmail.com>
2018-08-10 15:26:45 +0200

    buttons functionalities on click | date logic added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 //https://api.nasa.gov/planetary/apod?api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT
 //KEY: key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT
 
+//TODO
+// 1) function that will change date after pressing the button under the date
+// 2) RWD rethink
+//
+
 const apodUrl = 'https://api.nasa.gov/planetary/apod?api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT',
     roverUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
 
-let currentPicDate = '';
+//SETTING current pic date in the begening - as the current day's date
+let todaysDate = new Date();
+//changing format to ISO and triming string for foreward use
+todaysDate = todaysDate.toISOString().substr(0, 10);
+let currentPicDate = todaysDate;
 
 
 // ---------------------RUN
@@ -14,6 +23,20 @@ showAPOD();
 testDays();
 console.log(currentPicDate);
 
+
+//-------------------------------------DOM
+
+$('#date').text(currentPicDate);
+
+$('#nextBtn').on('click', () => {
+    addDay(currentPicDate);
+    $('#date').text(currentPicDate);
+});
+
+$('#prevBtn').on('click', () => {
+    substractDay(currentPicDate);
+    $('#date').text(currentPicDate);
+});
 
 //-----------------AJAX
 
@@ -33,7 +56,7 @@ function showAPOD(dateOfPic) {
 function testDays() {
     $.ajax({
         method: 'GET',
-        url: 'https://api.nasa.gov/planetary/apod?date=2014-10-01&api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT',
+        url: 'https://api.nasa.gov/planetary/apod?date=2014-9-1&api_key=wumtU3CAPhFX7hNuwpH1IA2EdrTVZoKohSz8fjtT',
         dataType: 'json',
     }).done(function (response) {
         // injectApod(response);
@@ -57,10 +80,6 @@ function showRoverAPI() {
     });
 }
 
-function changePic() {
-
-}
-
 
 //------------------------function declarations
 
@@ -68,18 +87,69 @@ function changePic() {
 function injectApod(apodObj) {
     var backgroundImg = apodObj.url,
         backgrounImgHd = apodObj.hdurl,
-        apodDate = apodObj.date,
+        // apodDate = apodObj.date,
         apodPictureTitle = apodObj.title,
         apodPictureExplanation = apodObj.explanation;
 
-    currentPicDate = apodDate;
+    // currentPicDate = apodDate;
 
     $('section.welcome').css('background-image', 'url(' + backgroundImg + ')');
     $('#picTitle').text(apodPictureTitle)
-    $('#date').text(apodDate);
+    // $('#date').text(apodDate);
     $('#explanation').text(apodPictureExplanation);
 
     console.log(currentPicDate);
     console.log([...currentPicDate]);
 
 }
+
+addDay = (dateIn) => {
+    let dt = new Date(dateIn);
+    //converted to the date object and add a day to it
+    dt.setDate(dt.getDate() + 1);
+
+    if (dt.getDate() < 10 && (dt.getMonth() + 1) < 10) {
+        date = dt.getFullYear() + "-" + '0' + (dt.getMonth() + 1) + '-' + '0' + (dt.getDate());
+
+    } else if (dt.getDate() < 10) {
+
+        date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + '-' + '0' + (dt.getDate());
+
+    } else if ((dt.getMonth() + 1) < 10) {
+
+        date = dt.getFullYear() + "-" + '0' + (dt.getMonth() + 1) + '-' + (dt.getDate());
+
+    } else {
+        date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + '-' + (dt.getDate());
+    }
+    console.log(date);
+    currentPicDate = date;
+}
+
+
+//Takes a currentPicDate, substract a day from it and set it as a new currentPicDate
+substractDay = (dateIn) => {
+    let dt = new Date(dateIn);
+    //converted to the date object and substract a day from it
+    dt.setDate(dt.getDate() - 1);
+
+    //check if day is less than 10 - then add "0" before it because getDate() method returnes
+    //day if less than 10  as : ex "09"-wanted value || "9" - returned value
+    // "06" , "07" "09" etc.  ISO format is needed to further comparison of the dates
+    if (dt.getDate() < 10 && (dt.getMonth() + 1) < 10) {
+        date = dt.getFullYear() + "-" + '0' + (dt.getMonth() + 1) + '-' + '0' + (dt.getDate());
+
+    } else if (dt.getDate() < 10) {
+
+        date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + '-' + '0' + (dt.getDate());
+
+    } else if ((dt.getMonth() + 1) < 10) {
+
+        date = dt.getFullYear() + "-" + '0' + (dt.getMonth() + 1) + '-' + (dt.getDate());
+
+    } else {
+        date = dt.getFullYear() + "-" + (dt.getMonth() + 1) + '-' + (dt.getDate());
+    }
+    console.log(date);
+    currentPicDate = date;
+};
